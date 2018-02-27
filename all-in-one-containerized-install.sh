20d1b9f2207f1b2abbe008563839c66dfc376b36
jay vyas <jay@apache.org>
2018-02-27 15:26:01 -0500

    Create all-in-one-containerized-install.sh

---
+++ all-in-one-containerized-install.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+echo "Enter your password for the hub:"
+read -s HUB_PASSWORD
+echo "Done..."
+#
+# Note that in production, you will want to encrypt the password, rather then inject it as a yml parameter.
+# Instructions will be added shortly.
+# 
+cat << EOF > config.yml
+apiVersion: v1
+kind: List
+metadata:
+  name: perceptor-configs
+items:
+- apiVersion: v1
+  kind: ConfigMap
+  metadata:
+    name: prometheus
+  data:
+    prometheus.yml: |
+      global:
+        scrape_interval: 5s
+      scrape_configs:
+      - job_name: 'perceptor-scrape'
+        scrape_interval: 5s
+        static_configs:
+        - targets: ['perceptor:3001', 'perceptor-scanner:3003'] # TODO Add perciever metrics here...
+- apiVersion: v1
+  kind: ConfigMap
+  metadata:
+    name: perceptor-scanner-config
+  data:
+    perceptor_scanner_conf.yaml: |
+      HubHost: "34.227.106.252.xip.io"
+      HubUser: "sysadmin"
+      HubUserPassword: "$HUB_PASSWORD"
+- apiVersion: v1
+  kind: ConfigMap
+  metadata:
+    name: kube-generic-perceiver-config
+  data:
+    perceiver.yaml: |
+      PerceptorHost: "perceptor"
+      PerceptorPort: 3001
+      AnnotationIntervalSeconds: 30
+      DumpIntervalMinutes: 30
+# TODO Replace w/ Secret creation.
+- apiVersion: v1
+  kind: ConfigMap
+  metadata:
+    name: perceptor-config
+  data:
+    perceptor_conf.yaml: |
+      HubHost: "34.227.106.252.xip.io"
+      HubUser: "sysadmin"
+      HubUserPassword: "$HUB_PASSWORD"
+- apiVersion: v1
+  kind: ConfigMap
+  metadata:
+    name: openshift-perceiver-config
+  data:
+    perceiver.yaml: |
+      PerceptorHost: "perceptor"
+      PerceptorPort: 3001
+      AnnotationIntervalSeconds: 30
+      DumpIntervalMinutes: 30
+EOF
+kubectl create -f config.yml
+
+echo "Your configuration is at config.yml, click enter to proceed installing, or edit it bbefore continuing"
+cat << EOF
+apiVersion: v1
+metadata:
+  name: "bds-perceptor components"
+  resourceVersion: "0.0.1"
+apiVersion: v1
+  kind: Pod
+  metadata:
+    name: protoform
+  spec:
+    metadata:
+      labels:
+        name: protoform
+      containers:
+      - name: protoform
+        image: gcr.io/gke-verification/blackducksoftware/protoform:latest
+        imagePullPolicy: Always
+        command: ./protoform
+        ports:
+        - containerPort: 3001
+          protocol: TCP
+        volumeMounts:
+        - name: protoform
+          mountPath: /etc/protoform
+        volumes:
+          - name: protoform
+            configMap:
+              name: protoform
+        restartPolicy: Never
+        dnsPolicy: ClusterFirst
+EOF
+kubectl create -f protoform.yml
