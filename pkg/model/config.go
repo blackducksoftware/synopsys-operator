8f8b4f4c52621e12f6da55c5904586d7fab57987
Senthil Murugan Manikantan <smanikantan@blackducksoftware.com>
2018-03-01 16:30:58 -0500

    Fixed the configmaps

--- pkg/model/config.go
+++ pkg/model/config.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"strconv"
 	"strings"
 
 	"k8s.io/api/core/v1"
@@ -62,14 +63,22 @@ func (p *ProtoformConfig) parameterize(json string) string {
 	if p.HubPort == 0 {
 		p.HubPort = 443
 	}
+	if p.DockerUsername == "" {
+		p.DockerUsername = "admin"
+	}
+	if p.DockerPasswordOrToken == "" {
+		panic("config failing: cannot continue without a Docker password!!!")
+	}
 	json = strings.Replace(json, "_1", p.PerceptorHost, n)
-	json = strings.Replace(json, "_2", string(p.PerceptorPort), n)
-	json = strings.Replace(json, "_3", string(p.AnnotationIntervalSeconds), n)
-	json = strings.Replace(json, "_4", string(p.DumpIntervalMinutes), n)
+	json = strings.Replace(json, "_2", strconv.Itoa(p.PerceptorPort), n)
+	json = strings.Replace(json, "_3", strconv.Itoa(p.AnnotationIntervalSeconds), n)
+	json = strings.Replace(json, "_4", strconv.Itoa(p.DumpIntervalMinutes), n)
 	json = strings.Replace(json, "_5", p.HubHost, n)
 	json = strings.Replace(json, "_6", p.HubUser, n)
 	json = strings.Replace(json, "_7", p.HubUserPassword, n)
-	json = strings.Replace(json, "_8", string(p.HubPort), n)
+	json = strings.Replace(json, "_8", strconv.Itoa(p.HubPort), n)
+	json = strings.Replace(json, "_9", p.DockerUsername, n)
+	json = strings.Replace(json, "_10", p.DockerPasswordOrToken, n)
 
 	return json
 }
@@ -85,6 +94,7 @@ func (p *ProtoformConfig) ToConfigMap() []*v1.ConfigMap {
 		"kube-generic-perceiver-config": "perceiver.yaml",
 		"perceptor-config":              "perceptor_conf.yaml",
 		"openshift-perceiver-config":    "perceiver.yaml",
+		"perceptor-imagefacade-config":  "perceptor_imagefacade_conf.yaml",
 	}
 
 	// Sed replace these.  Fine grained control over the json default format
@@ -112,11 +122,15 @@ func (p *ProtoformConfig) ToConfigMap() []*v1.ConfigMap {
 			"AnnotationIntervalSeconds": "_3",
 			"DumpIntervalMinutes": "_4"
 		}`,
-		"openshift_perceiver_config_t": `{
+		"openshift-perceiver-config": `{
 			"PerceptorHost": "_1",
 			"PerceptorPort": "_2",
 			"AnnotationIntervalSeconds": "_3",
 			"DumpIntervalMinutes": "_4"
+		}`,
+		"perceptor-imagefacade-config": `{
+			"DockerUser": "_9",
+			"DockerPassword": "_10"
 		}`,
 	}
 
@@ -126,7 +140,7 @@ func (p *ProtoformConfig) ToConfigMap() []*v1.ConfigMap {
 		contents := p.parameterize(defaults[config])
 		maps[x] = &v1.ConfigMap{
 			ObjectMeta: metav1.ObjectMeta{
-				Name: "prometheus",
+				Name: config,
 			},
 			Data: map[string]string{
 				filename: contents,
