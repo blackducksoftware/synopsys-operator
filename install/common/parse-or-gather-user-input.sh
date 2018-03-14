196381423aa0da936c3b093d5cbb97eb4f057f34
Senthil Murugan <msenmurugan@gmail.com>
2018-03-14 16:03:29 -0400

    Included the default value in interactive process

--- install/common/parse-or-gather-user-input.sh
+++ install/common/parse-or-gather-user-input.sh
@@ -10,10 +10,10 @@ function prompt() {
     echo "Interactive"
     echo "============================================"
     read -p "Hub server host (e.g. hub.mydomain.com:443): " _arg_hub_host_port
-    read -p "Hub user name (e.g. blackduck): " _arg_hub_user
+    read -p "Hub user name [sysadmin]: " _arg_hub_user
     read -sp "Hub user password : " _arg_hub_password
     echo " "
-    read -p "Maximum concurrent scans: " _arg_hub_max_concurrent_scans
+    read -p "Maximum concurrent scans: [7]" _arg_hub_max_concurrent_scans
     echo "============================================"
   else
     echo "Skipping prompts, --proto_prompty was turned off."
