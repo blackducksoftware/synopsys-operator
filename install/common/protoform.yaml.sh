a31639820c9618cc2e8afd8f20ec5913453b5c9c
Senthil <manikan@synopsys.com>
2018-03-28 14:50:57 -0400

    Added Viper secret
    * Added Viper secret
    * Removed Jay's last change

--- install/common/protoform.yaml.sh
+++ install/common/protoform.yaml.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+
 openshift="false"
 if [[ $_arg_image_perceiver == "on" ]] ; then
   openshift="true"
 fi
+
 DEF_PERCEPTOR_PROTOFORM_IMAGE=perceptor-protoform
 DEF_PERCEPTOR_PROTOFORM_TAG=master
 
 perceptor_protoform_image=${perceptor_protoform_image:-$DEF_PERCEPTOR_PROTOFORM_IMAGE}
 perceptor_protoform_tag=${perceptor_protoform_tag:-$DEF_PERCEPTOR_PROTOFORM_TAG}
+
+hubUserPassword=$(echo -n "$_arg_hub_password" | base64)
 
 cat << EOF > protoform.yaml
 apiVersion: v1
@@ -19,6 +23,9 @@ spec:
   - name: viper-input
     configMap:
       name: viper-input
+  - name: viper-secret
+    secret:
+      secretName: viper-secret
   containers:
   - name: protoform
     image: ${_arg_container_registry}/${_arg_image_repository}/${perceptor_protoform_image}:${perceptor_protoform_tag}
@@ -30,6 +37,9 @@ spec:
     volumeMounts:
     - name: viper-input
       mountPath: /etc/protoform/
+    - name: viper-secret
+      mountPath: /etc/protoform/
+      readOnly: true
   restartPolicy: Never
   serviceAccountName: protoform
   serviceAccount: protoform
@@ -37,43 +47,50 @@ spec:
 apiVersion: v1
 kind: List
 metadata:
-  name: viper-input
+  name: viper-inputs
 items:
+- apiVersion: v1
+  kind: Secret
+  metadata:
+    name: viper-secret
+  type: Opaque
+  data:
+    HubUserPassword: "$hubUserPassword"
+    abc: 123
 - apiVersion: v1
   kind: ConfigMap
   metadata:
     name: viper-input
   data:
-    protoform.yaml: |
-      DockerPasswordOrToken: "$_arg_private_registry_token"
-      HubHost: "$_arg_hub_host"
-      HubUser: "$_arg_hub_user"
-      HubPort: "$_arg_hub_port"
-      # TODO, inject as secret.
-      HubUserPassword: "$_arg_hub_password"
-      ConcurrentScanLimit: "$_arg_hub_max_concurrent_scans"
-      # TODO, the Docker username is hardcoded, it is not required as of now.
-      DockerUsername: "admin"
-      Namespace: "$_arg_pcp_namespace"
-      Openshift: "$openshift"
-      InternalDockerRegistries: "${_arg_private_registry[@]}"
-      DefaultCPU: "$_arg_container_default_cpu"
-      DefaultMem: "$_arg_container_default_memory"
+    DockerPasswordOrToken: "$_arg_private_registry_token"
+    HubHost: "$_arg_hub_host"
+    HubUser: "$_arg_hub_user"
+    HubPort: "$_arg_hub_port"
+    # TODO, inject as secret.
+    # HubUserPassword: "$_arg_hub_password"
+    ConcurrentScanLimit: "$_arg_hub_max_concurrent_scans"
+    # TODO, the Docker username is hardcoded, it is not required as of now.
+    DockerUsername: "admin"
+    Namespace: "$_arg_pcp_namespace"
+    Openshift: "$openshift"
+    InternalDockerRegistries: "${_arg_private_registry[@]}"
+    DefaultCPU: "$_arg_container_default_cpu"
+    DefaultMem: "$_arg_container_default_memory"
 
-      # TODO: Assuming for now that we run the same version of everything
-      # For the curated installers.  For developers ? You might want to
-      # hard code one of these values if using this script for dev/test.
-      Registry: "$_arg_container_registry"
-      ImagePath: "$_arg_image_repository"
-      Defaultversion: "$_arg_default_container_version"
-      PerceptorImageName: "$perceptor_image"
-      ScannerImageName: "$perceptor_scanner_image"
-      PodPerceiverImageName: "$pod_perceiver_image"
-      ImagePerceiverImageName: "$image_perceiver_image"
-      ImageFacadeImageName: "$perceptor_imagefacade_image"
-      PerceptorContainerVersion: "$perceptor_tag"
-      ScannerContainerVersion: "$perceptor_scanner_tag"
-      PerceiverContainerVersion: "$pod_perceiver_tag"
-      ImageFacadeContainerVersion: "$perceptor_imagefacade_tag"
-      LogLevel: "$_arg_container_default_log_level"
+    # TODO: Assuming for now that we run the same version of everything
+    # For the curated installers.  For developers ? You might want to
+    # hard code one of these values if using this script for dev/test.
+    Registry: "$_arg_container_registry"
+    ImagePath: "$_arg_image_repository"
+    Defaultversion: "$_arg_default_container_version"
+    PerceptorImageName: "$perceptor_image"
+    ScannerImageName: "$perceptor_scanner_image"
+    PodPerceiverImageName: "$pod_perceiver_image"
+    ImagePerceiverImageName: "$image_perceiver_image"
+    ImageFacadeImageName: "$perceptor_imagefacade_image"
+    PerceptorContainerVersion: "$perceptor_tag"
+    ScannerContainerVersion: "$perceptor_scanner_tag"
+    PerceiverContainerVersion: "$pod_perceiver_tag"
+    ImageFacadeContainerVersion: "$perceptor_imagefacade_tag"
+    LogLevel: "$_arg_container_default_log_level"
 EOF
