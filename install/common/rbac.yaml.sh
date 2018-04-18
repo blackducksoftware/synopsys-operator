7da9d296f0de588bec76e974685227b762afebdd
jay vyas <jay@apache.org>
2018-04-18 09:39:22 -0400

    Update rbac.yaml.sh

--- install/common/rbac.yaml.sh
+++ install/common/rbac.yaml.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 cat << EOF > rbac.yaml
-kind: ClusterRoleBinding
-apiVersion: rbac.authorization.k8s.io/v1beta1
-metadata:
-  name: protoform
-subjects:
-- kind: ServiceAccount
-  name: protoform
-  namespace: ${_arg_pcp_namespace}
-roleRef:
-  kind: ClusterRole
-  name: cluster-admin
-  apiGroup: ""
----
 apiVersion: v1
 kind: ServiceAccount
 metadata:
@@ -52,4 +39,17 @@ roleRef:
   kind: ClusterRole
   name: all-permission-on-pod-for-perceiver
   apiGroup: rbac.authorization.k8s.io
+---
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1beta1
+metadata:
+  name: protoform
+subjects:
+- kind: ServiceAccount
+  name: protoform
+  namespace: ${_arg_pcp_namespace}
+roleRef:
+  kind: ClusterRole
+  name: cluster-admin
+  apiGroup: ""
 EOF
