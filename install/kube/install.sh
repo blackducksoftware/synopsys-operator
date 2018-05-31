a0d3be7f635dfb2a16d3608b31843fdc12f5f267
Matt Fenwick <mfenwick100@gmail.com>
2018-05-31 14:04:12 -0400

    add exposed service type for metrics

--- install/kube/install.sh
+++ install/kube/install.sh
@@ -16,5 +16,5 @@ kubectl create -f protoform.yaml -n $_arg_pcp_namespace
 
 if [[ "$_arg_prometheus_metrics" == "on" ]] ; then
   kubectl create -f ../common/prometheus-deployment.yaml -n $_arg_pcp_namespace
-  kubectl expose service prometheus --port=9090 --name=prometheus-metrics -n $_arg_pcp_namespace
+  kubectl expose service prometheus --type=LoadBalancer --port=9090 --name=prometheus-metrics -n $_arg_pcp_namespace
 fi
