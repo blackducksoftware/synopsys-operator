55b3729e786ac93f62f0e9b755037ff2ade0dffd
Senthil Murugan Manikantan <msenmurugan@gmail.com>
2018-09-18 13:07:37 -0400

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -8,6 +8,6 @@ kubectl create ns $NS
 
 cat blackduck-protoform.yaml | sed 's/${REGISTRATION_KEY}/'$REG_KEY'/g' | sed 's/${NAMESPACE}/'$NS'/g' |sed 's/${BCH}/'${BRANCH}'/g' | kubectl create --namespace=$NS -f -
 
-kubectl expose rc blackduck-protoform --port=8080 --target-port=8080 --name=blackduck-protoform-np --type=NodePort --namespace=$NS
+#kubectl expose rc blackduck-protoform --port=8080 --target-port=8080 --name=blackduck-protoform-np --type=NodePort --namespace=$NS
 
-kubectl expose rc blackduck-protoform --port=8080 --target-port=8080 --name=blackduck-protoform-lb --type=LoadBalancer --namespace=$NS
+#kubectl expose rc blackduck-protoform --port=8080 --target-port=8080 --name=blackduck-protoform-lb --type=LoadBalancer --namespace=$NS
