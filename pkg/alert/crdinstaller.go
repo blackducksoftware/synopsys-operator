6547ec9075d4585faa3f22763a4707dd47cd1e81
Matt Fenwick <mfenwick100@gmail.com>
2018-10-29 13:59:58 -0400

    fix compile error

--- pkg/alert/crdinstaller.go
+++ pkg/alert/crdinstaller.go
@@ -57,7 +57,7 @@ type CRDInstaller struct {
 }
 
 // NewCRDInstaller will create a installer configuration
-func NewCRDInstaller(config *model.Config, kubeConfig *rest.Config, kubeClient *kubernetes.Clientset, defaults interface{}, stopCh <-chan struct{}) *CRDInstaller {
+func NewCRDInstaller(config *protoform.Config, kubeConfig *rest.Config, kubeClient *kubernetes.Clientset, defaults interface{}, stopCh <-chan struct{}) *CRDInstaller {
 	crdInstaller := &CRDInstaller{config: config, kubeConfig: kubeConfig, kubeClient: kubeClient, defaults: defaults, threadiness: config.Threadiness, stopCh: stopCh}
 	crdInstaller.resyncPeriod = 0
 	crdInstaller.indexers = cache.Indexers{}
