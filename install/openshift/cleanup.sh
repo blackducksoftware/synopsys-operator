7458edb64c767507ebad8a60f97c3474f137c05a
Matt Fenwick <mfenwick100@gmail.com>
2018-11-06 16:20:18 -0500

    clean up clusterroles, clusterrolebindings

--- install/openshift/cleanup.sh
+++ install/openshift/cleanup.sh
@@ -12,3 +12,10 @@ oc delete crd opssights.synopsys.com
 oc delete clusterrolebinding blackduck-operator-admin
 #oc delete clusterrolebinding protoform-admin
 #oc delete clusterrolebinding blackduck-operator-cluster-admin
+
+oc delete clusterrole skyfire
+oc delete clusterrole pod-perceiver
+
+oc delete clusterrolebinding skyfire
+oc delete clusterrolebinding perceptor-scanner
+oc delete clusterrolebinding pod-perceiver
