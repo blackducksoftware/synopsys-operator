cc31ab1e8734ee63ede25f9fba2dab9a8dc2f2e6
Jay Vyas <jvyas@jvyas-mac.internal.synopsys.com>
2018-11-07 09:27:57 -0500

    cleanup
    perceptor dev script

--- hack/local-up-perceptor.sh
+++ hack/local-up-perceptor.sh
@@ -2,6 +2,12 @@
 BDS=blackducksoftware
 
 function verify() {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "OS looks ok : $OSTYPE"
+    else
+        echo "this is a utility for mac dev only.  If you are hacking on linux, run the raw containers w/ makefile on a kube cluster!"
+        exit 2
+    fi
     if [[ "$GOPATH" == "" ]] ; 
         then echo "gopath not found: $GOPATHs" ; 
         exit 2
@@ -12,14 +18,18 @@ function verify() {
     else
         exit 2
     fi
+
+    if ! [ -x "$(command -v buffalo)" ]; then
+        echo "setting up buffalo"
+        brew install gobuffalo/tap/buffalo    
+    fi
 }
 
 function run() {
-    pushd $GOPATH/src/github.com/$BDS/perceptor-protoform/
-    go run cmd/protoform-installer/deployer.go
+    pushd $GOPATH/src/github.com/blackducksoftware/perceptor-protoform/cmd/operator-ui
+        buffalo dev
     popd
 }
 
-# startup
 verify
 run
