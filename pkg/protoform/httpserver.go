43db7844ee0c85a3ccf18dbed64cca45adc7bf34
Senthil Murugan <msenmurugan@gmail.com>
2018-11-08 00:38:42 -0500

    commented all GIN api's

--- pkg/protoform/httpserver.go
+++ pkg/protoform/httpserver.go
@@ -22,17 +22,11 @@ under the License.
 package protoform
 
 import (
-	"fmt"
-
-	"github.com/blackducksoftware/perceptor-protoform/pkg/api/hub/v1"
 	hubclientset "github.com/blackducksoftware/perceptor-protoform/pkg/hub/client/clientset/versioned"
-	"github.com/blackducksoftware/perceptor-protoform/pkg/util"
 	"github.com/gin-gonic/contrib/static"
 	gin "github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	log "github.com/sirupsen/logrus"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -59,79 +53,79 @@ func SetupHTTPServer(kubeClient *kubernetes.Clientset, hubClient *hubclientset.C
 			h.ServeHTTP(c.Writer, c.Request)
 		})
 
-		router.GET("/sql-instances", func(c *gin.Context) {
-			// keys := []string{"pvc-000", "pvc-001", "pvc-002"}
-			keys, _ := util.ListHubPV(hubClient, namespace)
-			c.JSON(200, keys)
-		})
+		// router.GET("/sql-instances", func(c *gin.Context) {
+		// 	// keys := []string{"pvc-000", "pvc-001", "pvc-002"}
+		// 	keys, _ := util.ListHubPV(hubClient, namespace)
+		// 	c.JSON(200, keys)
+		// })
 
-		router.GET("/storage-classes", func(c *gin.Context) {
-			var storageList map[string]string
-			storageList = make(map[string]string)
-			storageClasses, err := util.ListStorageClass(kubeClient)
-			if err != nil {
-				log.Errorf("unable to list the storage classes due to %+v", err)
-				c.JSON(404, fmt.Sprintf("\"message\": \"Failed to List the storage class due to %+v\"", err))
-			}
-			for _, storageClass := range storageClasses.Items {
-				storageList[storageClass.GetName()] = fmt.Sprintf("%s (%s)", storageClass.GetName(), storageClass.Provisioner)
-			}
-			storageList["none"] = fmt.Sprintf("%s (%s)", "None", "Disable dynamic provisioner")
-			c.JSON(200, storageList)
-		})
+		// router.GET("/storage-classes", func(c *gin.Context) {
+		// 	var storageList map[string]string
+		// 	storageList = make(map[string]string)
+		// 	storageClasses, err := util.ListStorageClass(kubeClient)
+		// 	if err != nil {
+		// 		log.Errorf("unable to list the storage classes due to %+v", err)
+		// 		c.JSON(404, fmt.Sprintf("\"message\": \"Failed to List the storage class due to %+v\"", err))
+		// 	}
+		// 	for _, storageClass := range storageClasses.Items {
+		// 		storageList[storageClass.GetName()] = fmt.Sprintf("%s (%s)", storageClass.GetName(), storageClass.Provisioner)
+		// 	}
+		// 	storageList["none"] = fmt.Sprintf("%s (%s)", "None", "Disable dynamic provisioner")
+		// 	c.JSON(200, storageList)
+		// })
 
-		router.GET("/hub", func(c *gin.Context) {
-			log.Debug("get hub request")
-			hubs, err := hubClient.SynopsysV1().Hubs(namespace).List(metav1.ListOptions{})
-			if err != nil {
-				log.Errorf("unable to get the hub list due to %+v", err)
-				c.JSON(404, "\"message\": \"Failed to List the hub\"")
-			}
+		// router.GET("/hub", func(c *gin.Context) {
+		// 	log.Debug("get hub request")
+		// 	hubs, err := hubClient.SynopsysV1().Hubs(namespace).List(metav1.ListOptions{})
+		// 	if err != nil {
+		// 		log.Errorf("unable to get the hub list due to %+v", err)
+		// 		c.JSON(404, "\"message\": \"Failed to List the hub\"")
+		// 	}
 
-			log.Debugf("found %d hubs", len(hubs.Items))
-			returnVal := make(map[string]v1.Hub)
+		// 	log.Debugf("found %d hubs", len(hubs.Items))
+		// 	returnVal := make(map[string]v1.Hub)
 
-			for _, v := range hubs.Items {
-				//l og.Debugf("hub %v: %+v", k, v)
-				returnVal[v.Spec.Namespace] = v
-			}
-			// log.Debugf("returnVal : %+v", returnVal)
-			c.JSON(200, returnVal)
-		})
+		// 	for _, v := range hubs.Items {
+		// 		//l og.Debugf("hub %v: %+v", k, v)
+		// 		returnVal[v.Spec.Namespace] = v
+		// 	}
+		// 	// log.Debugf("returnVal : %+v", returnVal)
+		// 	c.JSON(200, returnVal)
+		// })
 
-		router.POST("/hub", func(c *gin.Context) {
-			log.Debug("create hub request")
-			hubSpec := &v1.HubSpec{}
-			if err := c.BindJSON(hubSpec); err != nil {
-				log.Debugf("Fatal failure binding the incoming request ! %v", c.Request)
-			}
+		// router.POST("/hub", func(c *gin.Context) {
+		// 	log.Debug("create hub request")
+		// 	hubSpec := &v1.HubSpec{}
+		// 	if err := c.BindJSON(hubSpec); err != nil {
+		// 		log.Debugf("Fatal failure binding the incoming request ! %v", c.Request)
+		// 	}
 
-			ns, err := util.CreateNamespace(kubeClient, hubSpec.Namespace)
-			log.Debugf("created namespace: %+v", ns)
-			if err != nil {
-				log.Errorf("unable to create the namespace due to %+v", err)
-				c.JSON(404, "\"message\": \"Failed to create the namespace\"")
-				return
-			}
-			hubClient.SynopsysV1().Hubs(hubSpec.Namespace).Create(&v1.Hub{ObjectMeta: metav1.ObjectMeta{Name: hubSpec.Namespace}, Spec: *hubSpec})
+		// 	ns, err := util.CreateNamespace(kubeClient, hubSpec.Namespace)
+		// 	log.Debugf("created namespace: %+v", ns)
+		// 	if err != nil {
+		// 		log.Errorf("unable to create the namespace due to %+v", err)
+		// 		c.JSON(404, "\"message\": \"Failed to create the namespace\"")
+		// 		return
+		// 	}
+		// 	hubClient.SynopsysV1().Hubs(hubSpec.Namespace).Create(&v1.Hub{ObjectMeta: metav1.ObjectMeta{Name: hubSpec.Namespace}, Spec: *hubSpec})
 
-			c.JSON(200, "\"message\": \"Succeeded\"")
-		})
+		// 	c.JSON(200, "\"message\": \"Succeeded\"")
+		// })
 
-		router.DELETE("/hub", func(c *gin.Context) {
-			hubSpec := &v1.HubSpec{}
-			if err := c.BindJSON(hubSpec); err != nil {
-				log.Debugf("Fatal failure binding the incoming request ! %v", c.Request)
-				return
-			}
+		// router.DELETE("/hub", func(c *gin.Context) {
+		// 	hubSpec := &v1.HubSpec{}
+		// 	if err := c.BindJSON(hubSpec); err != nil {
+		// 		log.Debugf("Fatal failure binding the incoming request ! %v", c.Request)
+		// 		return
+		// 	}
 
-			log.Debugf("delete hub request %v", hubSpec.Namespace)
+		// 	log.Debugf("delete hub request %v", hubSpec.Namespace)
 
-			// This is on the event loop.
-			hubClient.SynopsysV1().Hubs(hubSpec.Namespace).Delete(hubSpec.Namespace, &metav1.DeleteOptions{})
+		// 	// This is on the event loop.
+		// 	hubClient.SynopsysV1().Hubs(hubSpec.Namespace).Delete(hubSpec.Namespace, &metav1.DeleteOptions{})
 
-			c.JSON(200, "\"message\": \"Succeeded\"")
-		})
+		// 	c.JSON(200, "\"message\": \"Succeeded\"")
+		// })
 
 		// Start and run the server - blocking call, obviously :)
 		router.Run(":8080")
