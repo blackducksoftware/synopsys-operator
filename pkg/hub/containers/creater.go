52c97c1ab22ce587fc956cbd7bf5000b9ec09f4b
Senthil Murugan <msenmurugan@gmail.com>
2018-11-14 11:19:36 -0500

    Fix postgres image name

--- pkg/hub/containers/creater.go
+++ pkg/hub/containers/creater.go
@@ -84,6 +84,9 @@ func (c *Creater) getFullContainerName(baseContainer string) string {
 			return reg
 		}
 	}
+	if strings.EqualFold(baseContainer, "postgres") {
+		return ""
+	}
 	img := fmt.Sprintf("docker.io/blackducksoftware/hub-%v:%v", baseContainer, c.hubSpec.HubVersion)
 	log.Warnf("Couldn't get container name for : %v, set it manually in the deployment, returning a reasonable default instead %v.", baseContainer, img)
 	log.Warn("In the future, you should provide fully qualified images for every single container when running the blackduck operator.")
