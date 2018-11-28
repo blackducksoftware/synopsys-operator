302a0cb03fa9845e1d266ed34104c80b6f052b3b
Jeremy Dartigalongue <jeremy.dartigalongue@outlook.com>
2018-11-28 13:04:25 +0000

    Prevent users from creating multiple instances of the operator on the same cluster

--- install/openshift/install.sh
+++ install/openshift/install.sh
@@ -11,6 +11,13 @@ VERSION=$3
 echo "Using the secret encoded in ../common/secret.yaml.  Edit the file before running, or press enter to continue with the defaults."
 read x
 
+CLUSTER_BINDING_NS=$(oc get clusterrolebindings synopsys-operator-admin -o go-template='{{range .subjects}}{{.namespace}}{{" "}}{{end}}' 2> /dev/null)
+if [[ $? -eq 0 ]]; then
+    SCRIPT_DIR=$(dirname "$0")
+    echo "You have already installed the operator in namespace $CLUSTER_BINDING_NS. Please run the cleanup script ($SCRIPT_DIR/cleanup.sh) before attempting to install the operator in $1"
+    exit 1
+fi
+
 oc new-project $NS
 
 oc create -f ../common/secret.yaml -n $NS
