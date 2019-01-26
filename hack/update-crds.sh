dfe8d5141423cc484dba68251bc9db643db523b8
Senthil Murugan <msenmurugan@gmail.com>
2019-01-25 20:48:40 -0500

    formatted the code

--- hack/update-crds.sh
+++ hack/update-crds.sh
@@ -11,18 +11,18 @@ pushd $GOPATH/src/k8s.io
   do
     git clone git@github.com:kubernetes/${REPO}.git
     pushd $REPO
-    git pull
+      git pull
     popd
   done
 popd 
 
 pushd $GOPATH/src/k8s.io/code-generator
-crds=( hub opssight alert )
-crdVersions=( v2 v1 v1 )
-j=0
+  crds=( hub opssight alert )
+  crdVersions=( v2 v1 v1 )
+  j=0
   for i in "${crds[@]}" ; do
-	set +x
-	./generate-groups.sh "deepcopy,client,informer,lister" "github.com/blackducksoftware/synopsys-operator/pkg/${i}/client" "github.com/blackducksoftware/synopsys-operator/pkg/api" ${i}:${crdVersions[j]}
+	  set +x
+	  ./generate-groups.sh "deepcopy,client,informer,lister" "github.com/blackducksoftware/synopsys-operator/pkg/${i}/client" "github.com/blackducksoftware/synopsys-operator/pkg/api" ${i}:${crdVersions[j]}
   	let "j++"
   done
 popd
