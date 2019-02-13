8274ffd2b783e12e8263b30c63d0b817caddee1d
Michael Hammer <mike.p.hammer@gmail.com>
2019-02-13 10:19:21 -0500

    fixed wrong package name in commandflags.go

--- cmd/synopsysctl/cmd/commandflags.go
+++ cmd/synopsysctl/cmd/commandflags.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package util
+package cmd
 
 import (
 	blackduckv1 "github.com/blackducksoftware/synopsys-operator/pkg/api/blackduck/v1"
