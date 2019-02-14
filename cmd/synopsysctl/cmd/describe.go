bf3c3a2b989495f1242498f2f24e7f416ab0a0d8
Michael Hammer <mike.p.hammer@gmail.com>
2019-02-14 10:11:56 -0500

    Updated Short description of edit and describe

--- cmd/synopsysctl/cmd/describe.go
+++ cmd/synopsysctl/cmd/describe.go
@@ -23,13 +23,7 @@ import (
 // describeCmd represents the describe command
 var describeCmd = &cobra.Command{
 	Use:   "describe",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Print a detailed description of the selected resource",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("describe called")
 	},
