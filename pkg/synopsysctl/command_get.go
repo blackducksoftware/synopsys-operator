f9e5785f264e149d4947a535a73eebdf07ced233
Michael Hammer <mike.p.hammer@gmail.com>
2019-02-19 15:57:14 -0500

    Improved description of get command

--- pkg/synopsysctl/command_get.go
+++ pkg/synopsysctl/command_get.go
@@ -23,13 +23,7 @@ import (
 // getCmd represents the get command
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List Synopsys Resources in your cluster",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("get called")
 	},
