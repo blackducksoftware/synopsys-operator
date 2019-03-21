f12abc37bf273835eaaac6b711138c5604a97375
Jeremy Dartigalongue <jeremy.dartigalongue@outlook.com>
2019-03-21 21:23:08 +0000

    Allow requests made using the synopsys-operator user

--- pkg/webhook/blackduck.go
+++ pkg/webhook/blackduck.go
@@ -5,6 +5,7 @@ import (
 	"github.com/blackducksoftware/synopsys-operator/pkg/api/blackduck/v1"
 	"github.com/sirupsen/logrus"
 	"k8s.io/api/admission/v1beta1"
+	v1beta12 "k8s.io/api/authentication/v1beta1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"net/http"
@@ -24,20 +25,33 @@ func (ow *OperatorWebhook) blackduckCustomResource(ar v1beta1.AdmissionReview) *
 		return ow.returnError(err.Error())
 	}
 
-	reviewResponse := v1beta1.AdmissionResponse{}
-	current, err := ow.blackduckClient.SynopsysV1().Blackducks(corev1.NamespaceDefault).Get(bd.Name, metav1.GetOptions{})
+	// Get TokenReview for current user
+	tok, err := ow.kubeClient.AuthenticationV1beta1().TokenReviews().Create(&v1beta12.TokenReview{Spec: v1beta12.TokenReviewSpec{Token: ow.kubeConfig.BearerToken}})
 	if err != nil {
-		logrus.Error(err)
 		return ow.returnError(err.Error())
 	}
 
-	if !reflect.DeepEqual(current.Status, bd.Status) {
-		logrus.Error(err)
-		return ow.returnError("Status cannot be modified")
-	}
+	// Log the request
+	logrus.Infof("Resource: %s, Operation: %s\nUsername: %s\n\n", ar.Request.Name, ar.Request.Operation, ar.Request.UserInfo.Username)
 
-	if strings.Compare(current.Spec.Namespace, bd.Spec.Namespace) != 0 {
-		return ow.returnError("Namespace cannot be modified")
+	reviewResponse := v1beta1.AdmissionResponse{}
+
+	// Only if the resource is being updated and that request was made by a different user
+	if ar.Request.Operation == v1beta1.Update && !strings.EqualFold(tok.Status.User.Username, ar.Request.UserInfo.Username) {
+		current, err := ow.blackduckClient.SynopsysV1().Blackducks(corev1.NamespaceDefault).Get(bd.Name, metav1.GetOptions{})
+		if err != nil {
+			logrus.Error(err)
+			return ow.returnError(err.Error())
+		}
+
+		if !reflect.DeepEqual(current.Status, bd.Status) {
+			logrus.Error(err)
+			return ow.returnError("Status cannot be modified")
+		}
+
+		if strings.Compare(current.Spec.Namespace, bd.Spec.Namespace) != 0 {
+			return ow.returnError("Namespace cannot be modified")
+		}
 	}
 
 	reviewResponse.Allowed = true
