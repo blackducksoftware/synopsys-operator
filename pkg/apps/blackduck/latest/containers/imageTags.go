0392c7de2ef9f16d3ffc61643a701525a01d26a1
Senthil Murugan Manikantan <msenmurugan@gmail.com>
2019-04-25 10:35:16 -0400

    Revert "Add support for blackduck 2019.4.1 (#224)"
    This reverts commit 3347febb3410c9f61f32ca2d7588732649c2489d.

--- pkg/apps/blackduck/latest/containers/imageTags.go
+++ pkg/apps/blackduck/latest/containers/imageTags.go
@@ -40,22 +40,6 @@ var imageTags = map[string]map[string]string{
 		"appcheck-worker":          "2019.01",
 		"rabbitmq":                 "1.0.0",
 	},
-	"2019.4.1": {
-		"blackduck-authentication": "2019.4.1",
-		"blackduck-documentation":  "2019.4.1",
-		"blackduck-jobrunner":      "2019.4.1",
-		"blackduck-registration":   "2019.4.1",
-		"blackduck-scan":           "2019.4.1",
-		"blackduck-webapp":         "2019.4.1",
-		"blackduck-cfssl":          "1.0.0",
-		"blackduck-logstash":       "1.0.4",
-		"blackduck-nginx":          "1.0.7",
-		"blackduck-solr":           "1.0.0",
-		"blackduck-zookeeper":      "1.0.0",
-		"blackduck-upload-cache":   "1.0.8",
-		"appcheck-worker":          "2019.01",
-		"rabbitmq":                 "1.0.0",
-	},
 }
 
 // GetImageTag returns the image tag of the given container
