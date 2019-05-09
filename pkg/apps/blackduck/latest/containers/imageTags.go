250aa56de1148c432a0dfbcd01f18531d343da4d
Jeremy Dartigalongue <jeremy.dartigalongue@outlook.com>
2019-05-09 17:23:35 -0400

    Add support for Blackduck 2019.4.2

--- pkg/apps/blackduck/latest/containers/imageTags.go
+++ pkg/apps/blackduck/latest/containers/imageTags.go
@@ -71,6 +71,22 @@ var imageTags = map[string]map[string]string{
 		"appcheck-worker":          "2019.01",
 		"rabbitmq":                 "1.0.0",
 	},
+	"2019.4.2": {
+		"blackduck-authentication": "2019.4.2",
+		"blackduck-documentation":  "2019.4.2",
+		"blackduck-jobrunner":      "2019.4.2",
+		"blackduck-registration":   "2019.4.2",
+		"blackduck-scan":           "2019.4.2",
+		"blackduck-webapp":         "2019.4.2",
+		"blackduck-cfssl":          "1.0.0",
+		"blackduck-logstash":       "1.0.4",
+		"blackduck-nginx":          "1.0.7",
+		"blackduck-solr":           "1.0.0",
+		"blackduck-zookeeper":      "1.0.0",
+		"blackduck-upload-cache":   "1.0.8",
+		"appcheck-worker":          "2019.01",
+		"rabbitmq":                 "1.0.0",
+	},
 }
 
 // GetImageTag returns the image tag of the given container
