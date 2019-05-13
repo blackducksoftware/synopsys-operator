fb4876a7ee62306a2892fc6bd6d8f0a7dacee065
Jeremy Dartigalongue <jeremy.dartigalongue@outlook.com>
2019-05-13 17:11:57 +0100

    Fix #330

--- pkg/protoform/kubeconfig.go
+++ pkg/protoform/kubeconfig.go
@@ -29,7 +29,7 @@ import (
 	"github.com/juju/errors"
 	log "github.com/sirupsen/logrus"
 	"k8s.io/client-go/kubernetes"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp" //for auths
+	_ "k8s.io/client-go/plugin/pkg/client/auth" //for auths
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
