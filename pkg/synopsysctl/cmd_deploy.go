e4fe163046e6ac18217b5b06cf612345cfeaf829
msenmurugan <msenmurugan@gmail.com>
2019-05-31 18:59:06 -0400

    change to 2019.4.2

--- pkg/synopsysctl/cmd_deploy.go
+++ pkg/synopsysctl/cmd_deploy.go
@@ -37,7 +37,7 @@ import (
 var exposeUI = ""
 var exposeMetrics = ""
 var deployNamespace = "synopsys-operator"
-var synopsysOperatorImage = "docker.io/blackducksoftware/synopsys-operator:2019.4.1"
+var synopsysOperatorImage = "docker.io/blackducksoftware/synopsys-operator:2019.4.2"
 var metricsImage = "docker.io/prom/prometheus:v2.1.0"
 var terminationGracePeriodSeconds int64 = 180
 var operatorTimeBombInSeconds int64 = 315576000
