7fcbb8e07ccb888ae3fcb8cc7bc45c4646374e02
msenmurugan <msenmurugan@gmail.com>
2019-06-19 11:01:33 -0400

    update default values

--- pkg/synopsysctl/constants.go
+++ pkg/synopsysctl/constants.go
@@ -22,10 +22,10 @@ under the License.
 package synopsysctl
 
 // SynopsysctlVersion is the current version of the synopsysctl utility
-const SynopsysctlVersion string = "2019.4.1"
+const SynopsysctlVersion string = "2019.6.0"
 
 // DefaultOperatorImage is Synopsys Operator image that is deployed by default
-const DefaultOperatorImage string = "gcr.io/saas-hub-stg/blackducksoftware/synopsys-operator:master"
+const DefaultOperatorImage string = "docker.io/blackducksoftware/synopsys-operator:2019.6.0"
 
 // DefaultMetricsImage is the Metrics image deployed with Synopsys Operator by default
 const DefaultMetricsImage string = "docker.io/prom/prometheus:v2.1.0"
