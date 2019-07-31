18d933134a2243ed600abb43d6e1eeee31bb5e15
Rob Rati <rrati@blackducksoftware.com>
2019-07-31 15:01:32 -0400

    Added missing copyright headers (#718)

--- pkg/apps/types/types.go
+++ pkg/apps/types/types.go
@@ -1,3 +1,24 @@
+/*
+Copyright (C) 2019 Synopsys, Inc.
+
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements. See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership. The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
 package types
 
 import (
@@ -21,10 +42,10 @@ type ServiceCreater func(*protoform.Config, *kubernetes.Clientset, interface{}) 
 // ConfigmapCreater refers to Replication Controller creater
 type ConfigmapCreater func(*protoform.Config, *kubernetes.Clientset, interface{}) (ConfigMapInterface, error)
 
-// PvcCreater refers to Replication Controller creater
+// PvcCreater refers to Persistent Volume Claim creater
 type PvcCreater func(*protoform.Config, *kubernetes.Clientset, interface{}) (PVCInterface, error)
 
-// SecretCreater refers to Replication Controller creater
+// SecretCreater refers to Secret creater
 type SecretCreater func(*protoform.Config, *kubernetes.Clientset, interface{}) (SecretInterface, error)
 
 // TagOrImage refers to Image and Tag
