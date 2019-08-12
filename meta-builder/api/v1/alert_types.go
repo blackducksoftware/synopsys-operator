9b002c962a5bbe56157bfd1158f8246cfa277512
Michael Hammer <mike.p.hammer@gmail.com>
2019-08-12 12:11:33 -0400

    initial alert controller structure (#742)

--- meta-builder/api/v1/alert_types.go
+++ meta-builder/api/v1/alert_types.go
@@ -23,17 +23,41 @@ import (
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // AlertSpec defines the desired state of Alert
+// Important: Run "make" to regenerate code after modifying this file
 type AlertSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	FinalYamlUrl   string `json:"finalYamlUrl"`
-	ShaOfFinalYaml string `json:"shaOfFinalYaml"`
+	Namespace         string   `json:"namespace,omitempty"`
+	Size              string   `json:"size"`
+	AlertImage        string   `json:"alertImage,omitempty"`
+	CfsslImage        string   `json:"cfsslImage,omitempty"`
+	ExposeService     string   `json:"exposeService"`
+	StandAlone        *bool    `json:"standAlone"`
+	Port              *int32   `json:"port"`
+	Environs          []string `json:"environs,omitempty"`
+	Secrets           []string `json:"secrets,omitempty"`
+	PersistentStorage bool     `json:"persistentStorage"`
+	PVCName           string   `json:"pvcName"`
+	PVCStorageClass   string   `json:"pvcStorageClass"`
+
+	// Should be passed like: e.g "1300Mi"
+	PVCSize               string                `json:"pvcSize"`
+	AlertMemory           string                `json:"alertMemory,omitempty"`
+	CfsslMemory           string                `json:"cfsslMemory,omitempty"`
+	DesiredState          string                `json:"desiredState,omitempty"`
+	ImageRegistries       []string              `json:"imageRegistries,omitempty"`
+	RegistryConfiguration RegistryConfiguration `json:"registryConfiguration,omitempty"`
+}
+
+// RegistryConfiguration contains the registry configuration
+type RegistryConfiguration struct {
+	Registry    string   `json:"registry"`
+	Namespace   string   `json:"namespace"`
+	PullSecrets []string `json:"pullSecrets"`
 }
 
 // AlertStatus defines the observed state of Alert
+// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+// Important: Run "make" to regenerate code after modifying this file
 type AlertStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 	State        string `json:"state"`
 	ErrorMessage string `json:"errorMessage"`
 }
