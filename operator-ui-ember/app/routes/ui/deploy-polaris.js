9686dc6a3741bd4351842200e90d89430bfb5a85
Michael Hammer <mike.p.hammer@gmail.com>
2019-08-29 14:14:52 -0400

    added page and AJAX for deploying Polaris

--- operator-ui-ember/app/routes/ui/deploy-polaris.js
+++ operator-ui-ember/app/routes/ui/deploy-polaris.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    actions: {
+        deployPolaris() {
+            alert("Running AJAX for Polaris...")
+            //alert(model.name)
+            var dataString = "Hello Polaris";
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:8081/",
+                data: dataString,
+                success: function () {
+                    alert("success")
+                }
+            });
+        }
+    }
 });
