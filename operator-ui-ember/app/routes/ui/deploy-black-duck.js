885ef3b6754c0a1a454277640ff503dec305a04f
Michael Hammer <mike.p.hammer@gmail.com>
2019-08-29 13:11:42 -0400

    jQuery and AJAX to process submitting a form

--- operator-ui-ember/app/routes/ui/deploy-black-duck.js
+++ operator-ui-ember/app/routes/ui/deploy-black-duck.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model() {
+        return [{
+            first: "black",
+            last: "duck"
+        },
+        {
+            first: "white",
+            last: "duck"
+        }];
+    }
 });
