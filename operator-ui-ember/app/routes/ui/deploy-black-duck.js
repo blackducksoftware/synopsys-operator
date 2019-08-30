fac7be60001076de7f1bfdd2503c9b6229d4afe4
Michael Hammer <mike.p.hammer@gmail.com>
2019-08-30 10:41:28 -0400

    reformatted polaris page and removed unessessary fields

--- operator-ui-ember/app/routes/ui/deploy-black-duck.js
+++ operator-ui-ember/app/routes/ui/deploy-black-duck.js
@@ -1,14 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    model() {
-        return [{
-            first: "black",
-            last: "duck"
-        },
-        {
-            first: "white",
-            last: "duck"
-        }];
-    }
 });
