340f9855d6a0b3ac697f5adcccddc6b4c9398c9c
Jeremy Dartigalongue <jeremy.dartigalongue@outlook.com>
2019-10-08 19:43:41 +0100

    Update the storage class (#961)

--- pkg/polaris/common_mutator.go
+++ pkg/polaris/common_mutator.go
@@ -22,8 +22,11 @@
 package polaris
 
 import (
+	appsv1 "k8s.io/api/apps/v1"
+	appsv1beta1 "k8s.io/api/apps/v1beta1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 // UpdatePersistentVolumeClaim updates the size for pvc
@@ -32,4 +35,24 @@ func UpdatePersistentVolumeClaim(pvc *corev1.PersistentVolumeClaim, size string)
 		pvc.Spec.Resources.Requests[corev1.ResourceStorage] = size
 	}
 	return nil
+}
+
+// patchStorageClass will iterate over the runtime objects and update the storage class
+func patchStorageClass(obj map[string]runtime.Object, storageClass string) {
+	if len(storageClass) > 0 {
+		for k, v := range obj {
+			switch v.(type) {
+			case *appsv1beta1.StatefulSet:
+				for claimTemplateIndex := range obj[k].(*appsv1beta1.StatefulSet).Spec.VolumeClaimTemplates {
+					obj[k].(*appsv1beta1.StatefulSet).Spec.VolumeClaimTemplates[claimTemplateIndex].Spec.StorageClassName = &storageClass
+				}
+			case *appsv1.StatefulSet:
+				for claimTemplateIndex := range obj[k].(*appsv1.StatefulSet).Spec.VolumeClaimTemplates {
+					obj[k].(*appsv1.StatefulSet).Spec.VolumeClaimTemplates[claimTemplateIndex].Spec.StorageClassName = &storageClass
+				}
+			case *corev1.PersistentVolumeClaim:
+				obj[k].(*corev1.PersistentVolumeClaim).Spec.StorageClassName = &storageClass
+			}
+		}
+	}
 }
