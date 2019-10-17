60c6c8d8bbe1da501933b591f150d52aea77df97
Senthil Murugan Manikantan <msenmurugan@gmail.com>
2019-10-17 13:11:43 -0400

    support for Black Duck 2019.10.0 (#994)

--- pkg/apps/blackduck/latest/containers/imageTags.go
+++ pkg/apps/blackduck/latest/containers/imageTags.go
@@ -24,6 +24,21 @@ package containers
 import "fmt"
 
 var imageTags = map[string]map[string]string{
+	"2019.10.0": {
+		"blackduck-authentication": "2019.10.0",
+		"blackduck-documentation":  "2019.10.0",
+		"blackduck-jobrunner":      "2019.10.0",
+		"blackduck-registration":   "2019.10.0",
+		"blackduck-scan":           "2019.10.0",
+		"blackduck-webapp":         "2019.10.0",
+		"blackduck-cfssl":          "1.0.0",
+		"blackduck-logstash":       "1.0.4",
+		"blackduck-nginx":          "1.0.9",
+		"blackduck-zookeeper":      "1.0.0",
+		"blackduck-upload-cache":   "1.0.9",
+		"appcheck-worker":          "2019.09",
+		"rabbitmq":                 "1.0.0",
+	},
 	"2019.8.1": {
 		"blackduck-authentication": "2019.8.1",
 		"blackduck-documentation":  "2019.8.1",
