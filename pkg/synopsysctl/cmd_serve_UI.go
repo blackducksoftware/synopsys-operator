eceaa10651b5d06aa5c49f29be006905c35fc6c8
Michael Hammer <mike.p.hammer@gmail.com>
2019-11-05 14:35:15 -0500

    add TLS flags and polaris license to UI

--- pkg/synopsysctl/cmd_serve_UI.go
+++ pkg/synopsysctl/cmd_serve_UI.go
@@ -332,11 +332,13 @@ type PolarisUIRequestResponse struct {
 	PostgresContainer bool   `json:"postgresContainer"`
 	PostgresSSLMode   string `json:"postgresSSLMode"`
 
-	SMTPHost        string `json:"smtpHost"`
-	SMTPPort        string `json:"smtpPort"`
-	SMTPUsername    string `json:"smtpUsername"`
-	SMTPPassword    string `json:"smtpPassword"`
-	SMTPSenderEmail string `json:"smtpSenderEmail"`
+	SMTPHost                   string `json:"smtpHost"`
+	SMTPPort                   string `json:"smtpPort"`
+	SMTPUsername               string `json:"smtpUsername"`
+	SMTPPassword               string `json:"smtpPassword"`
+	SMTPSenderEmail            string `json:"smtpSenderEmail"`
+	SMTPTLSTrustedHosts        string `json:"smtpTLSTrustedHosts"`
+	SMTPTLSCheckServerIdentity bool   `json:"smtpTLSCheckServerIdentity"`
 
 	UploadServerSize   string `json:"uploadServerSize"`
 	EventstoreSize     string `json:"eventstoreSize"`
@@ -351,7 +353,9 @@ type PolarisUIRequestResponse struct {
 	OrganizationAdminName     string `json:"organizationAdminName"`
 	OrganizationAdminUsername string `json:"organizationAdminUsername"`
 	OrganizationAdminEmail    string `json:"organizationAdminEmail"`
-	CoverityLicensePath       string `json:"coverityLicensePath"`
+
+	CoverityLicensePath string `json:"coverityLicensePath"`
+	PolarisLicensePath  string `json:"polarisLicensePath"`
 }
 
 // // TODO: add Black Duck to UI for 12.0 release
@@ -595,6 +599,13 @@ func convertPolarisUIResponseToPolarisObject(polarisObj *polaris.Polaris, polari
 		}
 		polarisObj.Licenses.Coverity = CoverityLicense
 	}
+	if !updating || polarisUIRequestConfig.PolarisLicensePath != "" {
+		PolarisLicense, err := util.ReadFileData(polarisUIRequestConfig.PolarisLicensePath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read polaris license from file: %+v", err)
+		}
+		polarisObj.Licenses.Polaris = PolarisLicense
+	}
 
 	// If the user is updating then overwrite the old values even if these fields are empty
 	// If the user is creating then only overwrite the default value if the new value is not empty
@@ -659,6 +670,8 @@ func convertPolarisUIResponseToPolarisObject(polarisObj *polaris.Polaris, polari
 	polarisObj.PolarisDBSpec.SMTPDetails.Username = polarisUIRequestConfig.SMTPUsername
 	polarisObj.PolarisDBSpec.SMTPDetails.Password = polarisUIRequestConfig.SMTPPassword
 	polarisObj.PolarisDBSpec.SMTPDetails.SenderEmail = polarisUIRequestConfig.SMTPSenderEmail
+	polarisObj.PolarisDBSpec.SMTPDetails.TLSTrustedHosts = polarisUIRequestConfig.SMTPTLSTrustedHosts
+	polarisObj.PolarisDBSpec.SMTPDetails.TLSCheckServerIdentity = polarisUIRequestConfig.SMTPTLSCheckServerIdentity
 
 	// CONFIGURE STORAGE
 	// use defaults/previous values if they aren't provided
@@ -707,6 +720,7 @@ func convertPolarisObjToUIResponse(polarisObj polaris.Polaris) (*PolarisUIReques
 	// synopsysctl does not save the file paths so the data - the data is not sent to the UI
 	polarisUIRequestConfig.GCPServiceAccountPath = ""
 	polarisUIRequestConfig.CoverityLicensePath = ""
+	polarisUIRequestConfig.PolarisLicensePath = ""
 
 	polarisUIRequestConfig.ImagePullSecrets = polarisObj.ImagePullSecrets
 	polarisUIRequestConfig.Registry = polarisObj.Registry
@@ -728,6 +742,8 @@ func convertPolarisObjToUIResponse(polarisObj polaris.Polaris) (*PolarisUIReques
 	polarisUIRequestConfig.SMTPUsername = polarisObj.PolarisDBSpec.SMTPDetails.Username
 	polarisUIRequestConfig.SMTPPassword = polarisObj.PolarisDBSpec.SMTPDetails.Password
 	polarisUIRequestConfig.SMTPSenderEmail = polarisObj.PolarisDBSpec.SMTPDetails.SenderEmail
+	polarisUIRequestConfig.SMTPTLSTrustedHosts = polarisObj.PolarisDBSpec.SMTPDetails.TLSTrustedHosts
+	polarisUIRequestConfig.SMTPTLSCheckServerIdentity = polarisObj.PolarisDBSpec.SMTPDetails.TLSCheckServerIdentity
 
 	// STORAGE
 	polarisUIRequestConfig.UploadServerSize = polarisObj.PolarisDBSpec.UploadServerDetails.Storage.StorageSize
