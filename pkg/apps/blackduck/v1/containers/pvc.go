46ccf76371cba31dac533ad6f2d9fc104385620e
Michael Hammer <mike.p.hammer@gmail.com>
2019-11-20 10:33:59 -0500

    added solr back to black duck v1

--- pkg/apps/blackduck/v1/containers/pvc.go
+++ pkg/apps/blackduck/v1/containers/pvc.go
@@ -32,6 +32,7 @@ func (c *Creater) GetPVCs() ([]*components.PersistentVolumeClaim, error) {
 		"blackduck-authentication": "2Gi",
 		"blackduck-cfssl":          "2Gi",
 		"blackduck-registration":   "2Gi",
+		"blackduck-solr":           "2Gi",
 		"blackduck-webapp":         "2Gi",
 		"blackduck-logstash":       "20Gi",
 		"blackduck-zookeeper":      "4Gi",
