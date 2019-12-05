14fa90fe46d24f90ea835be794e5791fb751a851
msenmurugan <msenmurugan@gmail.com>
2019-12-05 00:26:37 -0500

    get and update master key command changes

--- pkg/blackduck/ctl_blackduck_test.go
+++ pkg/blackduck/ctl_blackduck_test.go
@@ -182,6 +182,7 @@ func TestAddCRSpecFlagsToCommand(t *testing.T) {
 	cmd.Flags().StringVar(&ctl.NodeAffinityFilePath, "node-affinity-file-path", ctl.NodeAffinityFilePath, "Absolute path to a file containing a list of node affinities")
 	cmd.Flags().StringVar(&ctl.Registry, "registry", ctl.Registry, "Name of the registry to use for images e.g. docker.io/blackducksoftware")
 	cmd.Flags().StringSliceVar(&ctl.PullSecrets, "pull-secret-name", ctl.PullSecrets, "Only if the registry requires authentication")
+	cmd.Flags().StringVar(&ctl.SealKey, "seal-key", ctl.SealKey, "Seal key to encrypt the master key when Source code upload is enabled")
 
 	// TODO: Remove this flag in next release
 	cmd.Flags().MarkDeprecated("desired-state", "desired-state flag is deprecated and will be removed by the next release")
@@ -692,6 +693,16 @@ func TestSetCRSpecFieldByFlag(t *testing.T) {
 				LicenseKey:    "changed",
 			},
 			changedSpec: &blackduckapi.BlackduckSpec{LicenseKey: "changed"},
+		},
+		// case
+		{
+			flagName:   "seal-key",
+			initialCtl: NewCRSpecBuilderFromCobraFlags(),
+			changedCtl: &CRSpecBuilderFromCobraFlags{
+				blackDuckSpec: &blackduckapi.BlackduckSpec{},
+				SealKey:       "changed",
+			},
+			changedSpec: &blackduckapi.BlackduckSpec{SealKey: util.Base64Encode([]byte("changed"))},
 		},
 	}
 
