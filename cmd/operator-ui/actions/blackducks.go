6ea63427ff68c6c9aca7b950ae135fc1066f41e1
operator-r2d2 <50117352+operator-r2d2@users.noreply.github.com>
2020-02-03 12:15:55 -0500

    vendor operator 2019.12.1 to operator ui (#1205)
    Co-authored-by: Senthil Murugan Manikantan <msenmurugan@gmail.com>

--- cmd/operator-ui/actions/blackducks.go
+++ cmd/operator-ui/actions/blackducks.go
@@ -117,7 +117,7 @@ func (v BlackducksResource) New(c buffalo.Context) error {
 	blackduck := &blackduckapi.Blackduck{}
 	blackduck.Spec = *blackduckSpec
 	// TODO: fix the sort logic for Black Duck version
-	blackduck.Spec.Version = "2019.12.0"
+	blackduck.Spec.Version = "2019.12.1"
 	if v.config.IsClusterScoped {
 		blackduck.Spec.Namespace = ""
 	} else {
