cb173e4e00e629b35975184bf235144f6136dfba
msenmurugan <msenmurugan@gmail.com>
2020-02-13 19:43:59 -0500

    Support for Alert 5.2.0

--- pkg/apps/alert/latest/alert_versions.go
+++ pkg/apps/alert/latest/alert_versions.go
@@ -53,6 +53,10 @@ var imageTags = map[string]map[string]string{
 		"blackduck-alert": "5.1.0",
 		"blackduck-cfssl": "1.0.0",
 	},
+	"5.2.0": {
+		"blackduck-alert": "5.2.0",
+		"blackduck-cfssl": "1.0.0",
+	},
 }
 
 // GetImageTag returns the url for an image
